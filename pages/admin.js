d8a90061bc6684cae58208167626bc530238cf00
Yilun Xu <allenxu0124@gmail.com>
2026-02-18 02:15:04 +0800

    Fix import path for supabase in admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from '../lib/supabase.js';
 
 export default function Admin(){
   const [orders,setOrders] = useState([]);
